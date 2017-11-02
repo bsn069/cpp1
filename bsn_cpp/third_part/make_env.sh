3633af2a87608d97700f9a5824582b33c6041d90
putao <513026809@qq.com>
2017-11-02 20:52:43 +0800

    试试

--- bsn_cpp/third_part/make_env.sh
+++ bsn_cpp/third_part/make_env.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
+# run in docker
 
 if [ ! -d "boost" ]; then
 	cp /usr/include/boost boost -R
 fi
-
-if [ ! -f "LuaJIT-2.0.5.tar.gz" ]; then
-	curl http://luajit.org/download/LuaJIT-2.0.5.tar.gz -o LuaJIT-2.0.5.tar.gz
-fi
  
 if [ ! -d "LuaJIT" ]; then
+	if [ ! -f "LuaJIT-2.0.5.tar.gz" ]; then
+		curl http://luajit.org/download/LuaJIT-2.0.5.tar.gz -o LuaJIT-2.0.5.tar.gz
+	fi
 	tar -xzvf LuaJIT-2.0.5.tar.gz
 	mv LuaJIT-2.0.5 LuaJIT
 fi
@@ -17,18 +17,19 @@ if [ ! -d "LuaBridge" ]; then
 	git clone https://github.com/bsn069/LuaBridge.git
 fi
 
-if [ ! -f "protobuf-cpp-3.4.1.tar.gz" ]; then
-	curl https://github.com/google/protobuf/releases/download/v3.4.1/protobuf-cpp-3.4.1.tar.gz -o protobuf-cpp-3.4.1.tar.gz
-fi
- 
-if [ ! -d "protobuf-cpp" ]; then
-	tar -xzvf protobuf-cpp-3.4.1.tar.gz
-	mv protobuf-cpp-3.4.1 protobuf-cpp
-fi
-
 if [ ! -d "pbc" ]; then
 	git clone https://github.com/bsn069/pbc.git
 fi
- 
- 
 
+if [ ! -d "protobuf" ]; then
+	git clone https://github.com/google/protobuf.git
+fi
+ 
+ 
+# if [ ! -d "protobuf-cpp" ]; then
+# 	if [ ! -f "protobuf-cpp-3.4.1.tar.gz" ]; then
+# 		curl https://github.com/google/protobuf/releases/download/v3.4.1/protobuf-cpp-3.4.1.tar.gz -o protobuf-cpp-3.4.1.tar.gz
+# 	fi
+# 	tar -xzvf protobuf-cpp-3.4.1.tar.gz
+# 	mv protobuf-cpp-3.4.1 protobuf-cpp
+# fi

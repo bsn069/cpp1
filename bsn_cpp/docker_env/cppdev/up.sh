6260646c3353d54bdd3cd6be64963a19dbeec539
putao <513026809@qq.com>
2017-12-18 16:15:48 +0800

    ty

--- bsn_cpp/docker_env/cppdev/up.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-#  gdb error add seccomp=unconfined
-docker run -it --security-opt seccomp=unconfined -v  /media:/media bsn069/cppdev
--- bsn_cpp/docker_env/docker_rm.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker rm -f `docker ps -a -q`
-

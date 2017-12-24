a8a6885627d9ed9810198f163732f050e9f5a015
bsn_macpro <513026809@qq.com>
2017-12-24 23:56:44 +0800

    a

--- bsn_cpp/vm_env.sh
+++ bsn_cpp/vm_env.sh
@@ -5,4 +5,8 @@ if [ ! -d "/root/bsn_cpp" ]; then
 	if [ -d "/media/sf_/github/cpp1/bsn_cpp" ]; then
 		ln -s /media/sf_/github/cpp1/bsn_cpp /root/bsn_cpp
 	fi
+
+	if [ -d "/media/psf/Home/Documents/pt/github/cpp1/bsn_cpp" ]; then
+		ln -s /media/psf/Home/Documents/pt/github/cpp1/bsn_cpp /root/bsn_cpp
+	fi
 fi
